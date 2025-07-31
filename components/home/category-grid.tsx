d559fe6bd7a0f99117bcaf4642284f127ee52ee0
adamssemakula <adamssemakula@gmail.com>
2025-07-31 07:00:44 +0000

    fixed categories grid column

--- components/home/category-grid.tsx
+++ components/home/category-grid.tsx
@@ -63,7 +63,7 @@ export default function CategoryGrid() {
         </p>
       </div>
 
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-6 items-center">
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-6 items-center">
         {categories.map((category) => {
           const IconComponent = category.icon
           return (
